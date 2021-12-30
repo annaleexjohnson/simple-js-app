06b64ed4e4857af8de26050c01c17af8df2f8101
annaleexjohnson <87035551+annaleexjohnson@users.noreply.github.com>
2021-12-29 19:33:21 -0600

    feat: add first 3 objects to pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-alert('Hello World');
-
-var favFood = 'Fried Chicken';
-document.write(favFood);
+var pokemonList = [
+    { name: 'Bulbasaur', height: 0.7, types:['grass', 'poison'] },
+    { name: 'Charmander', height: 0.6, types: ['fire'] },
+    { name: 'Squirtle', height: 0.5, types: ['water'] }
+];
