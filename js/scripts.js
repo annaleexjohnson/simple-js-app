657fc37ddb7553974c95900ab6cb3babf99a7aec
annaleexjohnson <87035551+annaleexjohnson@users.noreply.github.com>
2022-01-01 12:49:47 -0600

    feat: add document.write for height/types

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,14 @@ var pokemonList = [
     { name: 'Charmander', height: 0.6, types: ['fire'] },
     { name: 'Squirtle', height: 0.5, types: ['water'] }
 ];
+
+for (var i = 0; i < pokemonList.length; i++){
+    if(pokemonList[i].height >= 0.7){
+        document.write("Name: " + pokemonList[i].name + " (Height: " + pokemonList[i].height + ")" +"- Wow, that's big!" + "<br>");
+        document.write("Type(s): " + pokemonList[i].types + "<br>");
+    } 
+    else{
+        document.write("Name: " + pokemonList[i].name + " (Height: " + pokemonList[i].height + ")" + "<br>"); 
+        document.write("Type(s): " + pokemonList[i].types + "<br>");
+    }
+}
